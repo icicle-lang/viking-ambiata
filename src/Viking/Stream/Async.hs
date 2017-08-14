be488e81f65a318dae64939c3fb9f89e645548c4
Jacob Stanley <jacob@stanley.io>
2017-08-14 15:17:11 +1000

    Add mapConcurrently / mapConcurrentlyN over streams

---
+++ src/Viking/Stream/Async.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+module Viking.Stream.Async (
+    mapConcurrently
+  , mapConcurrentlyN
+  ) where
+
+import           Control.Concurrent (getNumCapabilities)
+import qualified Control.Concurrent.Async.Lifted as Async
+import           Control.Monad.Base (liftBase)
+import           Control.Monad.Trans.Control (MonadBaseControl)
+
+import           P
+
+import           System.IO (IO)
+
+import           Viking
+import qualified Viking.Stream as Stream
+
+
+mapConcurrentlyN :: MonadBaseControl IO m => Int -> (a -> m b) -> Stream (Of a) m r -> Stream (Of b) m r
+mapConcurrentlyN n f =
+  Stream.concat .
+  Stream.mapM (Async.mapConcurrently f) .
+  Stream.mapped Stream.toList .
+  Stream.chunksOf n
+
+mapConcurrently :: MonadBaseControl IO m => (a -> m b) -> Stream (Of a) m r -> Stream (Of b) m r
+mapConcurrently f xs = do
+  n <- liftBase getNumCapabilities
+  mapConcurrentlyN n f xs
